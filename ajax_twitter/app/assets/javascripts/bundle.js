728d7acd00b48755fa7c5fa70c31ad956af127a3
Kevin <NateraKevin1@gmail.com>
2022-05-25 19:05:35 -0400

    added render func

--- ajax_twitter/app/assets/javascripts/bundle.js
+++ ajax_twitter/app/assets/javascripts/bundle.js
@@ -12,6 +12,15 @@ class FollowToggle  {
         this.el = $(el);
         this.userId = this.el.data("userId");
         this.followState =  this.el.data("initialFollowState");
+        this.render();
+    }
+
+    render(){
+        if(this.followState === "unfollowed") {
+          this.el.text("Follow!");
+        } else {
+          this.el.text("Unfollow!");
+        }
     }
 
 };
--- ajax_twitter/frontend/follow_toggle.js
+++ ajax_twitter/frontend/follow_toggle.js
@@ -3,6 +3,15 @@ class FollowToggle  {
         this.el = $(el);
         this.userId = this.el.data("userId");
         this.followState =  this.el.data("initialFollowState");
+        this.render();
+    }
+
+    render(){
+        if(this.followState === "unfollowed") {
+          this.el.text("Follow!");
+        } else {
+          this.el.text("Unfollow!");
+        }
     }
 
 };
