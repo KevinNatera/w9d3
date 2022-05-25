67d2ad4fe7f0629f5567ddf471f0cb3c200f9b5d
Kevin <NateraKevin1@gmail.com>
2022-05-25 19:01:43 -0400

    updated twitter.js

---
+++ ajax_twitter/frontend/twitter.js
@@ -0,0 +1,8 @@
+const FollowToggle = require("./follow_toggle");
+
+$(() => {
+    const buttons = $('button.follow-toggle');
+    buttons.each((index, ele) => {
+      new FollowToggle(ele);
+    })
+})
