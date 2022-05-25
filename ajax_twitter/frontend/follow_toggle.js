57d0dd6c6c79874a10f8cd0a702905ba4dad0af5
Kevin <NateraKevin1@gmail.com>
2022-05-25 18:59:18 -0400

    created FollowToggle class

---
+++ ajax_twitter/frontend/follow_toggle.js
@@ -0,0 +1,11 @@
+class FollowToggle  {
+    constructor(el) {
+        this.el = $(el);
+        this.userId = this.el.data("userId");
+        this.followState =  this.el.data("initialFollowState");
+    }
+
+};
+
+
+module.exports = FollowToggle;
